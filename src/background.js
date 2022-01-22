779c08b604b8bf8809a4a729d4b4ae9754edd445
Dhruman Gupta <31363068+DhrumanGupta@users.noreply.github.com>
2022-01-22 16:49:24 +0530

    Add enabled state and writing

--- src/background.js
+++ src/background.js
@@ -1,24 +1,7 @@
-// const findAllURL = text => {
-//     const current = window.location.href;
-//     if (current.startsWith(text)) {
-//         document.documentElement.innerHTML = 'Domain is blocked';
-//         document.documentElement.scrollTop = 0;
-//     }
-// }
-//
-// findAllURL("https://www.facebook.com/");
-// findAllURL("https://www.instagram.com/");
-// findAllURL("https://www.netflix.com/");
-// findAllURL("https://www.youtube.com/");
-
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
+    chrome.storage.sync.get(["blocked"], function (local) {
         if (!Array.isArray(local.blocked)) {
-            chrome.storage.local.set({ blocked: [] });
-        }
-
-        if (typeof local.enabled !== "boolean") {
-            chrome.storage.local.set({ enabled: false });
+            chrome.storage.sync.set({ blocked: [] });
         }
     });
 });
@@ -32,9 +15,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     const hostname = new URL(url).hostname;
 
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-        const { blocked, enabled } = local;
-        if (Array.isArray(blocked) && enabled && blocked.find(domain => hostname.includes(domain))) {
+    chrome.storage.sync.get(["blocked", "isWriting"], function (local) {
+        const { blocked, isWriting } = local;
+        console.log(local)
+        if (Array.isArray(blocked) && isWriting && blocked.find(domain => hostname.includes(domain))) {
             chrome.tabs.executeScript(tabId, {
                 file: 'blockpage.js'
             });
--- src/popup.js
+++ src/popup.js
@@ -1,23 +1,64 @@
-const textarea = document.getElementById("textarea");
-const save = document.getElementById("save");
-const checkbox = document.getElementById("checkbox");
+// const textarea = document.getElementById("textarea");
+// const save = document.getElementById("save");
+// const checkbox = document.getElementById("checkbox");
+const startButton = document.getElementById("start");
 
-save.addEventListener("click", () => {
-    const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
-    chrome.storage.local.set({ blocked });
-});
+// save.addEventListener("click", () => {
+// const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
+// chrome.storage.local.set({ blocked });
+// });
 
-checkbox.addEventListener("change", (event) => {
-    const enabled = event.target.checked;
-    chrome.storage.local.set({ enabled });
+// checkbox.addEventListener("change", (event) => {
+// const enabled = event.target.checked;
+// chrome.storage.local.set({ enabled });
+// });
+
+let isWriting = false;
+
+const stoppedClassName = "text-emerald-500 hover:bg-emerald-500 hover:text-emerald-100".split(' ')
+const runningClassName = "text-emerald-100 bg-emerald-500 hover:bg-emerald-600 shadow-emerald-400/30".split(' ')
+
+const setButtonClass = () => {
+    if (isWriting) {
+        runningClassName.forEach(x => {
+            startButton.classList.add(x)
+        })
+        stoppedClassName.forEach(x => {
+            startButton.classList.remove(x)
+        })
+        startButton.innerText = "Exit Writing Mode"
+    } else {
+        runningClassName.forEach(x => {
+            startButton.classList.remove(x)
+        })
+        stoppedClassName.forEach(x => {
+            startButton.classList.add(x)
+        })
+        startButton.innerText = "Enter Writing Mode"
+    }
+}
+
+startButton.onclick = (event) => {
+    chrome.storage.sync.set({isWriting: !isWriting})
+}
+
+chrome.storage.onChanged.addListener(function (changes) {
+    if (!changes.isWriting) {
+        return;
+    }
+    isWriting = changes.isWriting.newValue;
+
+    setButtonClass()
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-        const { blocked, enabled } = local;
+    chrome.storage.sync.get(["blocked", "isWriting"], (local) => {
+        const {blocked, isWriting: writing} = local;
+        isWriting = writing;
+        setButtonClass()
+
         if (Array.isArray(blocked)) {
             textarea.value = blocked.join("\n");
-            checkbox.checked = enabled;
         }
     });
 });
