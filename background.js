2e2ce79c255b8e6638bf1f15fa08ded2b89a82c1
Vikram <vikram.jaisingh123@gmail.com>
2022-01-22 13:05:44 +0530

    ugh google is bad

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) { 
-    return {cancel: true}; 
-},
-{urls: ["https://www.netflix.com", "http://www.youtube.com/*"]},
-["blocking"]);
+findAllURL = function changeAllURL(text){
+    var current = window.location.href;
+    if(current.startsWith(text)){
+      document.documentElement.innerHTML = '';
+      document.documentElement.innerHTML = 'Domain is blocked';
+      document.documentElement.scrollTop = 0;
+    }
+}
+
+findAllURL("https://www.facebook.com/");
+findAllURL("https://www.instagram.com/");
+findAllURL("https://www.netflix.com/");
+findAllURL("https://www.youtube.com/");
